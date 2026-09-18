230b37c9844d52c8c9356776e8a1e30f33751f55
Nikola Topalov <99753964+topalovniki@users.noreply.github.com>
2026-09-18 22:44:05 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/* CPG Design configuration */
-const CPG_CONFIG = {
-  contactEmail: "topalovniki@gmail.com",
-  phone: "+359887352008",
-  // FormSubmit receives the form and forwards it to the email above.
-  formEndpoint: "https://formsubmit.co/ajax/topalovniki@gmail.com"
-};
-
