c11d55a9e2d606655638db4f93d777392150b4d7
Nikola Topalov <99753964+topalovniki@users.noreply.github.com>
2026-09-18 20:58:37 +0300

    Update formEndpoint to use contact email

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const CPG_CONFIG = {
   phone: "+359887352008",
   // Optional: if you use a form service, put its endpoint here.
   // Example: https://formspree.io/f/xxxxxxxx
-  formEndpoint: ""
+  formEndpoint: "topalovniki@gmail.com"
 };
